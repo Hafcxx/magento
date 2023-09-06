c1dcef787d0983c4c8438a9f001185c034a3bff8
Claudio Aniñir <claudio.aninir01@gmail.com>
2023-09-06 18:49:29 -0300

    actualizacion taller 2

---
+++ app/code/Taller/TareaDos/Controller/Index/save.php
@@ -0,0 +1,47 @@
+<?php
+namespace Taller\TareaDos\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Taller\TareaDos\Model\Tarjeta;
+use Magento\Framework\Controller\ResultFactory;
+
+class Save extends Action
+{
+    protected $tarjeta;
+
+    public function __construct(
+        Context $context,
+        Tarjeta $tarjeta
+    ) {
+        parent::__construct($context);
+        $this->Tarjeta = $tarjeta;
+    }
+
+    public function execute()
+    {
+
+        if ($this->getRequest()->isPost()) {
+            /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+            $resultRedirect = $this->resultRedirectFactory->create();
+            try {
+                $data = $this->getRequest()->getPostValue();
+
+                $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+                $result->setData(['message' => $data]);
+                
+
+                if (!empty($data)) {
+                    $this->Tarjeta->setData($data);
+                    $this->Tarjeta->save();
+                    //return $result;
+                    $this->messageManager->addSuccessMessage(__('Data saved successfully.'));
+                    return $resultRedirect->setPath('*/*');
+                }
+            } catch (\Exception $e) {
+                $this->messageManager->addErrorMessage(__('An error occurred while saving data.'));
+                return $resultRedirect->setPath('*/*');
+            }
+        }
+    }
+}
